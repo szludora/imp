1bf6aed58a4f67c8cd4de42d616fd395dba3019d
SzluDora <szlucska.dora@gmail.com>
2024-09-16 15:00:40 +0200

    if

---
+++ gyak2/if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*#include <stdbool.h>*/
+
+int main(){
+    int x;
+    x = 42;
+    if(x % 2)
+        printf("Páratlan\n");
+    else
+        printf("Páros\n");
+    return 0;
+}
